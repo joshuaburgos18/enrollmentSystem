1683d6c8a420463389724d363746916e6ec1726b
joshuaburgos18 <mbburgos@up.edu.ph>
2021-03-07 22:07:09 +0800

    Setup Eureka server and Service registration of Student Service

--- student/src/main/java/org/enrollmentsystem/student/StudentApplication.java
+++ student/src/main/java/org/enrollmentsystem/student/StudentApplication.java
@@ -2,10 +2,12 @@ package org.enrollmentsystem.student;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-public class Application {
+@EnableDiscoveryClient
+public class StudentApplication {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(StudentApplication.class, args);
     }
 }
